8222794d08ce229eb1805790a3a04328cf6cd604
VolchikG <nik5102006@gmail.com>
2026-03-18 09:19:36 +0000

    test5

---
+++ task_5.py
@@ -0,0 +1,11 @@
+load = [10, 50, 20, 80, 30, 90, 40, 60, 10, 20]
+print(f"Исходная нагрузка: {load}")
+
+smoothed_load = []
+
+for i in range(len(load)-2):
+    window = load[i:i+3]
+    average = sum(window) // 3
+    smoothed_load.append(average)
+
+print(f"Сглаженный тренд: {smoothed_load}")
